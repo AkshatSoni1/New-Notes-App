1a294251e66676014d93829f26bcdc30ce08b271
AkshatSoni1 <sakshat551@gmail.com>
2023-12-23 16:56:06 +0530

    Fixed some css

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 "use client"
 import Link from "next/link"
-import {useContext } from "react"
+import { useContext } from "react"
 import { AppContext } from "@/context/AppContext/page"
 import { VscAccount } from "react-icons/vsc";
 import { useState } from "react";
@@ -9,85 +9,85 @@ import ShowToast from "@/helper/page";
 
 const Navbar = () => {
 
-    let {isLoggedIn, setIsLoggedIn, setUser} = useContext(AppContext);
-    const [toggleDropdown, setToggleDropdown] = useState(false);
+  let { isLoggedIn, setIsLoggedIn, setUser } = useContext(AppContext);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
-    // const pathname = usePathname();
+  // const pathname = usePathname();
 
-    // const noNavPages = [
-    //     "/login",
-    //     "/signup"
-    //   ]
+  // const noNavPages = [
+  //     "/login",
+  //     "/signup"
+  //   ]
 
-    const handleLogout = () => {
-      try {
-        localStorage.removeItem('token');
-        setIsLoggedIn(false)
-        setUser(null)
-        setToggleDropdown(false);
-        ShowToast(true, "Log out successful!")
-      } catch (error) {
-        console.log(error)
-        ShowToast(false, "Logged out unsuccessful!")
-      }
+  const handleLogout = () => {
+    try {
+      localStorage.removeItem('token');
+      setIsLoggedIn(false)
+      setUser(null)
+      setToggleDropdown(false);
+      ShowToast(true, "Log out successful!")
+    } catch (error) {
+      console.log(error)
+      ShowToast(false, "Logged out unsuccessful!")
     }
+  }
   return (
     <>
-    {/* {(noNavPages.includes(pathname))
+      {/* {(noNavPages.includes(pathname))
         ?
         <div className=""></div>
         : */}
-    <nav className="flex justify-between px-10 md:px-20 py-5 bg-gray-900 items-center">
+      <nav className="flex justify-between px-10 md:px-20 py-5 bg-gray-900 items-center">
         <div className="">
-            <Link href={isLoggedIn?"/":"/login"}
-                className=" text-lg font-semibold bg-gradient-to-r from-sky-300 via-cyan-600 to-purple-400 inline-block text-transparent bg-clip-text tracking-[0.3rem]"
-            >
-                Note Verse
-            </Link>
+          <Link href={isLoggedIn ? "/" : "/login"}
+            className=" text-lg font-semibold bg-gradient-to-r from-sky-300 via-cyan-600 to-purple-400 inline-block text-transparent bg-clip-text tracking-[0.3rem]"
+          >
+            Note Verse
+          </Link>
         </div>
         <div className="hidden sm:flex gap-4">
-            {isLoggedIn ?
+          {isLoggedIn ?
             <>
-            <Link
+              <Link
                 href={"/create-note"}
                 className="text-gray-900 text-base font-bold tracking-wider bg-gradient-to-r from-sky-300 via-cyan-500 to-purple-300 hover:bg-gradient-to-br focus:ring-2 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 rounded-lg px-4 py-2 text-center"
-            >
+              >
                 Create Note
-            </Link>
-            <Link
+              </Link>
+              <Link
                 href={"/"}
                 className="text-gray-900 text-base font-bold tracking-wider bg-gradient-to-r from-sky-300 via-cyan-500 to-purple-300 hover:bg-gradient-to-br focus:ring-2 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 rounded-lg px-4 py-2 text-center"
                 onClick={handleLogout}
-            >
+              >
                 Log out
-            </Link>
+              </Link>
             </>
             :
             <>
-            <Link
-            href={"/login"}
-            className="text-gray-900 text-base font-bold tracking-wider bg-gradient-to-r from-sky-300 via-cyan-500 to-purple-300 hover:bg-gradient-to-br focus:ring-2 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 rounded-lg px-4 py-2 text-center"
-        >
-            Login
-        </Link>
-            <Link
+              <Link
+                href={"/login"}
+                className="text-gray-900 text-base font-bold tracking-wider bg-gradient-to-r from-sky-300 via-cyan-500 to-purple-300 hover:bg-gradient-to-br focus:ring-2 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 rounded-lg px-4 py-2 text-center"
+              >
+                Login
+              </Link>
+              <Link
                 href={"/signup"}
                 className="text-gray-900 text-base font-bold tracking-wider bg-gradient-to-r from-sky-300 via-cyan-500 to-purple-300 hover:bg-gradient-to-br focus:ring-2 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 rounded-lg px-4 py-2 text-center"
-            >
+              >
                 Sign-up
-            </Link>
+              </Link>
             </>
-            }
+          }
         </div>
         <div className="flex sm:hidden">
-        <VscAccount 
+          <VscAccount
             className="text-cyan-200 text-3xl cursor-pointer"
             onClick={() => setToggleDropdown((prev) => !prev)}
-        />
+          />
 
-        {toggleDropdown && (<div className="bg-gradient-to-r from-sky-50 to-purple-50 z-30 text-center absolute right-9 top-11 mt-3 w-1/2 p-5 rounded-lg min-w-[210px] flex-col gap-2 justify-end items-end max-h-fit shadow-md shadow-black py-6">
-        {isLoggedIn ?
-        <>
+          {toggleDropdown && (<div className="bg-gradient-to-r from-sky-50 to-purple-50 z-30 text-center absolute right-10 top-11 mt-3 w-1/2 p-5 rounded-lg min-w-[210px] flex-col gap-2 justify-end items-end max-h-fit shadow-sm shadow-slate-400 py-6">
+            {isLoggedIn ?
+              <>
                 <Link
                   href="/create-note"
                   className=" text-blue-950  font-semibold text-xl hover:text-cyan-500 text-center [text-shadow:_0_1px_3px_rgb(0_0_0_/_40%)]"
@@ -102,9 +102,9 @@ const Navbar = () => {
                 >
                   Log Out
                 </button>
-        </>
-:
-<div className="flex flex-col items-center">
+              </>
+              :
+              <div className="flex flex-col items-center">
                 <Link
                   href="/login"
                   className="text-gray-900 font-bold text-xl hover:text-cyan-500 text-center [text-shadow:_0_1px_3px_rgb(0_0_0_/_40%)]"
@@ -120,12 +120,12 @@ const Navbar = () => {
                 >
                   Sign Up
                 </Link>
-</div>
- } </div>)
-}
-</div>
-    </nav>
-    {/* } */}
+              </div>
+            } </div>)
+          }
+        </div>
+      </nav>
+      {/* } */}
     </>
   )
 }
